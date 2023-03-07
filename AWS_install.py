c1ae30737e344f52c3757a9e077114ce144398b0
kjkostlan <kkostla1@jhu.edu>
2023-03-06 23:32:12 -0800

    bugfix

--- AWS_install.py
+++ AWS_install.py
@@ -1,5 +1,5 @@
 # Paste this into the AWS cloud shell to install. Go line-by-line or all at once.
-# There is no indentation since that would interfere with the paste-in.
+# There is *no indentation* since that would interfere with the paste-in.
 # Run the skythonic() function will automatically run everything.
 python3=1 # Bash/python polyglot fun (works whether you are Python or bash, and leaves the CLI in Python).
 python3
@@ -14,7 +14,8 @@ f1 = lambda: [os.remove(file) if os.path.exists(file) else False for file in fil
 f2 = lambda: [os.system(' '.join(['curl','-o',f,root+f])) for f in files]
 f3 = lambda: [import1(mdname) for mdname in module_strs] #https://stackoverflow.com/questions/301134/how-can-i-import-a-module-dynamically-given-its-name-as-string
 f4 = lambda: [importlib.reload(sys.modules[mdname]) for mdname in module_strs]
-skythonic = lambda:[f0(), f1(), f2(), f3(), f4()]
-f0(); f1(); f2(); f3(); f4()
+do = lambda f,g: g() if f() != '___' else None # do in order, which may be affecting the curl refreshing.
+skythonic = lambda: do(lambda: do(lambda: do(f0,f1), lambda: do(f2,f3)), f4)
+skythonic()
 ls = lambda: os.system('ls') # TODO: better check if it worked.
 print('Installation hopefully worked!')
