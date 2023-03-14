92493dfe8a153d512b9c7d6ea51fa5fdcf13a6cb
kjkostlan <kostlankj@gmail.com>
2023-03-13 20:33:33 -0700

    id2obj and obj2id functions to convert between a string and description dict.

--- AWS/AWS_core.py
+++ AWS/AWS_core.py
@@ -33,24 +33,53 @@ def create(type, name, **kwargs):
     return x
 
 def obj2id(obj_desc): # Gets the ID from the object.
+    #(This is a bit tricky since some descs have multible ids)
     if type(obj_desc) is str:
         return obj_desc
     id = None
     avoid = {'DhcpOptionsId','OwnerId','AvailabilityZoneId', 'ImageId'} # TODO: more will be needed.
-    kys = obj_or_id.keys()
+    priority = ['InstanceId', 'RouteTableId', 'SubnetId'] # Order matters here for objects with multible Id's.
+    for kp in priority:
+        if kp in obj_desc:
+            return obj_desc[kp]
+    if 'eni-' in str(obj_desc):
+        print('eni- bearing:', obj_desc)
+    kys = obj_desc.keys()
     for ky in kys:
         if ky not in avoid and ky.endswith('Id'):
-            id = obj_or_id[ky]
+            id = obj_desc[ky]
             break
     if id is None:
         raise Exception("Can't extract the Id.")
     return id
 
+def id2obj(id):
+    if type(id) is dict:
+        return id # Already a description.
+    if id.startswith('igw-'):
+        return ec2c.describe_internet_gateways(InternetGatewayIds=[id])['InternetGateways'][0]
+    elif id.startswith('vpc-'):
+        return ec2c.describe_vpcs(VpcIds=[id])['Vpcs'][0]
+    elif id.startswith('subnet-'):
+        return ec2c.describe_subnets(SubnetIds=[id])['Subnets'][0]
+    elif id.startswith('key-'): #Only needs the name.
+        return ec2c.describe_key_pairs(KeyPairIds=[id])['KeyPairs'][0]
+    elif id.startswith('sg-'):
+        return ec2c.describe_security_groups(GroupIds=[id])['SecurityGroups'][0]
+    elif id.startswith('rtb-'):
+        return ec2c.describe_route_tables(RouteTableIds=[id])['RouteTables'][0]
+    elif id.startswith('i-'):
+        return ec2c.describe_instances(InstanceIds=[id])['Instances'][0]
+    elif id.startswith('addr-'):
+        return ec2c.describe_addresses(AddressIds=[id])['Addresses'][0]
+    else:
+        raise Exception('TODO: handle this case:', id)
+
 def delete(desc_or_id): # Delete an object given an id OR a description dict.
     id = obj2id(desc_or_id)
     if id.startswith('igw-'):
         attchs = ec2c.describe_internet_gateways(InternetGatewayIds=[id])['InternetGateways'][0]['Attachments']
-        for attch in attchs: # Must detach before deletion.
+        for attch in attchs: # Must detach before deletion. TODO: detaching for everything.
             ec2c.detach_internet_gateway(InternetGatewayId=id, VpcId=attch['VpcId'])
         ec2c.delete_internet_gateway(InternetGatewayId=id)
     elif id.startswith('vpc-'):
