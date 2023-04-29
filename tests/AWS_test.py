eb8c4552a9d7ebee9bbc41f816c16c2506a79214
kjkostlan <kostlankj@gmail.com>
2023-04-29 13:08:06 -0700

    Added tests of the Jumpbox VM setup.

--- tests/AWS_test.py
+++ tests/AWS_test.py
@@ -1,28 +1,78 @@
 import AWS.AWS_query as AWS_query
 import vm
 
-def _cmd_test(the_cmd, look_for, vm_id):
-    tubo = vm.patient_ssh_pipe(vm_id, printouts=True)
+class test_results:
+    def __init__(self, name=None):
+        self.results = []
+        self.details = []
+        self.names = []
+        self.name = name
+    def failures(self):
+        out = []
+        for i in range(len(self.results)):
+            if not self.results[i]:
+                out.append(self.details[i])
+        return out
+    def all_passed(self):
+        return len(self.failures())==0
+    def add_test(self, result, details, name=None):
+        self.results.append(result)
+        self.details.append(details)
+        self.names.append(name)
+
+    def __str__(self):
+        fs = self.failures()
+        n = len(self.results)
+        if len(fs)==0:
+            return f'{self.name}: all {n} tests passed!'
+        else:
+            return f'{self.name}: {len(fs)}/{n} tests failed.'
+    def __repr__(self):
+        return '<test_results '+str(self)+'>'
+    def report(self, verbose=True):
+        if self.all_passed():
+            return 'No failures to report.'
+        lines = []
+        for i in range(len(self.results)):
+            if not self.results[i]:
+                lines.append(self.names[i]+(': '+str(self.details[i]).replace('\r\n','\n') if verbose else ''))
+        return '\n'.join(lines)
+
+def _cmd_test(results, the_cmd, look_for, vm_id, test_name):
+    print('Testing cmd on jumpbox: '+test_name)
+    tubo = vm.patient_ssh_pipe(vm_id, printouts=False)
+
+    #tubo.send('echo AWS_test')
+    #import time
+    #time.sleep(1.0)
+    #print('BLitty:', tubo.blit())
+    #TODO
+
+    out, err, _ = tubo.API('echo AWS_test')
+    tubo.empty(True)
     tubo.send(the_cmd)
     vm.laconic_wait(tubo, 'testing', timeout_seconds=24)
-    x = tubo.blit()
-    print('X is:', x)
+    pipe_dump = tubo.blit()
+    tubo.close()
+    passed = look_for in pipe_dump
+    results.add_test(passed, pipe_dump, test_name)
 
 def test_ssh_jumpbox():
     # Tests: A: is everything installed?
     #        B: Are the scp files actually copied over?
+    out = test_results('jumpbox_tests')
     vm_desc = AWS_query.get_by_name('machine', 'BYOC_jumpbox_VM')
     if vm_desc is None:
         raise Exception('Cant find BYOC_jumpbox_VM to test on. Is it named differently or not set up?')
 
-    _cmd_test('ping', 'foo', vm_desc)
-    _cmd_test('aws TODO', look_for, vm_desc)
-    _cmd_test(cmd, look_for, vm_desc)
+    _cmd_test(out, 'ping', 'ping: usage error: Destination address required', vm_desc, 'Test_ping')
+    _cmd_test(out, 'aws ec2 describe-subnets', 'BYOC_jumpbox_subnet', vm_desc, 'Test_AWS_descsubnets')
 
-    #TODO our_vm_id
+    _cmd_test(out, 'cd ~/Skythonic\n ls -a', 'eye_term.py', vm_desc, 'test files skythonic')
+    _cmd_test(out, 'cd ~/Skythonic/softwareDump\n ls -a', 'QWERTY', vm_desc, 'test files to softwareDump')
+    #_cmd_test(out, cmd, look_for, vm_desc)
 
-    print(vm_desc)
-    return False
+    return out
 
 def test_new_machine_and_jumpbox():
     #Tests: A: Make a machine in the jumpbox and ssh to it.
